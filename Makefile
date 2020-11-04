9c6de4a27b35544c84b7e56a3b721ba99fe47f3b
Kirill Chertkov <chertkov.k@gmail.com>
2020-11-05 02:11:31 +0300

    fix bug with retrieving user info, edit makefile

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ build:
 	docker build -t hexlet/slackonar:latest .
 
 save:
-	docker run -v $(CURDIR):/slackonar --env-file ./.env -it hexlet/slackonar:latest $(first) $(last)
+	docker run --rm -v $(CURDIR):/slackonar --env-file ./.env -it hexlet/slackonar:latest $(first) $(last)
--- slackonar.py
+++ slackonar.py
@@ -13,32 +13,32 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 def get_crumb():
     headers = {
-        'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36",
+        'user-agent': USER_AGENT,
     }
     # get the login form text for crumb parsing
     form_text = requests.get(WORKSPACE_URL, headers=headers).text
     # search the crumb string
-    crumb_string = re.search("(?<=crumbValue&quot;:&quot;)(.*?)&quot;", form_text).group(1)
+    crumb_string = re.search('(?<=crumbValue&quot;:&quot;)(.*?)&quot;', form_text).group(1)
     # urlencode it
-    crumb = urllib.parse.quote(crumb_string).replace("-%5Cu2603", "-%E2%98%83")
+    crumb = urllib.parse.quote(crumb_string).replace('-%5Cu2603', '-%E2%98%83')
     return crumb
 
 
 def get_cookie():
-    payload = "signin=1&crumb={}&email={}&password={}".format(CRUMB, USER, PASSWORD)
+    payload = f'signin=1&crumb={CRUMB}&email={USER}&password={PASSWORD}'
     headers = {
-        'content-type': "application/x-www-form-urlencoded",
-        'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
+        'content-type': 'application/x-www-form-urlencoded',
+        'user-agent': USER_AGENT,
     }
     response = requests.post(WORKSPACE_URL, data=payload, headers=headers, allow_redirects=False)
 
     for cookie in re.split(' ', response.headers['Set-Cookie']):
-        if "d=" in cookie:
+        if 'd=' in cookie:
             return cookie
 
 
 def get_token():
-    url = "https://app.slack.com/auth?app=client"
+    url = 'https://app.slack.com/auth?app=client'
 
     headers = {
         'cookie': COOKIE
@@ -55,7 +55,7 @@ def get_token():
 
 
 def get_messages(channel_id, first_message_time, last_message_time):
-    url = "{}/api/conversations.history".format(WORKSPACE_URL)
+    url = f'{WORKSPACE_URL}/api/conversations.history'
 
     multipart_data = MultipartEncoder(
         fields={
@@ -68,25 +68,25 @@ def get_messages(channel_id, first_message_time, last_message_time):
     )
 
     headers = {
-        "Content-Type": multipart_data.content_type,
+        'Content-Type': multipart_data.content_type,
         'cookie': COOKIE,
     }
 
     get_msgs = requests.post(url, data=multipart_data, headers=headers, verify=False)
-    messages = get_msgs.json()["messages"]
+    messages = get_msgs.json()['messages']
 
     return messages
 
 
 def get_messages_from_thread(channel_id, thread_id, replies):
-    url = '{}/api/conversations.replies'.format(WORKSPACE_URL)
+    url = f'{WORKSPACE_URL}/api/conversations.replies'
     # messages limit
     limit = replies
 
-    data = "channel={}&ts={}&limit={}&token={}".format(channel_id, thread_id, limit, TOKEN)
-    
+    data = f'channel={channel_id}&ts={thread_id}&limit={limit}&token={TOKEN}'
+
     headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
+        'Content-Type': 'application/x-www-form-urlencoded',
         'cookie': COOKIE
     }
 
@@ -96,31 +96,35 @@ def get_messages_from_thread(channel_id, thread_id, replies):
 
 
 def get_user_info_by_id(user_id):
-    url = '{}/api/users.profile.get'.format(WORKSPACE_URL)
+    url = f'{WORKSPACE_URL}/api/users.profile.get'
 
-    data = "user={}&token={}".format(user_id, TOKEN)
-    
+    data = f'user={user_id}&token={TOKEN}'
+
     headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
+        'Content-Type': 'application/x-www-form-urlencoded',
         'cookie': COOKIE
     }
 
     user_info = requests.get(url, headers=headers, params=data)
-
     return user_info.json()
 
 
 def id2name(matchobj):
-    id = re.sub(r'[\W_]+', '', matchobj.group(0))
-    return f"_{get_user_info_by_id(id)['profile']['real_name']}_"
+    user_id = re.sub(r'[\W_]+', '', matchobj.group(0))
+    user_info = get_user_info_by_id(user_id)
+    if 'profile' in user_info['profile'].keys():
+        return f'_{user_info["profile"]["profile"]["real_name"]}_'
+    else:
+        return f'_{user_info["profile"]["real_name"]}_'
 
 
 def formatter(message):
     # get message author name
-    name = get_user_info_by_id(message['user'])['profile']['real_name']
-    # check if message contain only technical info
-    if 'subtype' in message.keys():
-        return
+    user_info = get_user_info_by_id(message['user'])
+    if 'profile' in user_info['profile'].keys():
+        name = user_info['profile']['profile']['real_name']
+    else:
+        name = user_info['profile']['real_name']
     # fix markdown sintax for correct pdf rendering
     message_text = re.sub('```', '\n```\n', message['text'])
     # replace id in mentions to user names
@@ -139,16 +143,23 @@ def formatter(message):
         for file in message['files']:
             if 'mimetype' in file.keys():
                 if 'image' in file['mimetype']:
-                    image_urls.append(file["url_private"])
+                    image_urls.append(file['url_private'])
         for url in image_urls:
             filename = download_image(url)
             message_text = f'{message_text}\n\n![](images/{filename})'
-    # add name and indent, if message is a part of thread
+    # add name
+    message_text = f'\n**{name}**: {message_text}\n'
+    # add indent, if message is a part of thread
     if 'parent_user_id' in message.keys():
         message_text = message_text.replace('\n', '\n> ')
-        message_text = f'\n> **{name}**: {message_text}\n'
-    else:
-        message_text = f'\n**{name}**: {message_text}\n'
+        message_text = f'{message_text}\n'
+    # check if message contain only technical info
+    if 'subtype' in message.keys():
+        if message['subtype'] == 'thread_broadcast':
+            message_text = message_text.replace('\n', '\n> ')
+            message_text = f'{message_text}\n'
+        else:
+            return
     return message_text
 
 
@@ -170,13 +181,12 @@ def download_image(url):
     return f'{filename}.{extension}'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # hide warnings about certificate verification
     logging.captureWarnings(True)
 
     # create dirs
     os.makedirs('/slackonar/slackonars/images', exist_ok=True)
-
 
     first_message_url = sys.argv[1].split('/')
     workspace = first_message_url[2]
@@ -188,6 +198,7 @@ if __name__ == "__main__":
         last_message_url = sys.argv[2].split('/')
         last_message_time = re.search('p([0-9].*)', last_message_url[5]).group(1)
 
+    USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
     USER = os.environ['EMAIL']
     PASSWORD = os.environ['PASSWORD']
     WORKSPACE_URL = f'https://{workspace}'
@@ -195,7 +206,7 @@ if __name__ == "__main__":
     COOKIE = get_cookie()
     TOKEN = get_token()
 
-    filename = f"{'{:%d.%m.%Y-%H:%M:%S}'.format(datetime.datetime.now())}.md"
+    filename = f'{datetime.datetime.now().strftime("%d.%m.%Y-%H:%M:%S")}.md'
     for message in reversed(get_messages(channel_id, first_message_time, last_message_time)):
         # for messages with thread
         if 'thread_ts' and 'reply_count' in message.keys():
